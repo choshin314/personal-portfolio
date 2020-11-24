a2b7284c8b347e4415ce03255447a4dd59ea232a
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-11-24 14:07:00 -0600

    update default styles, add styled section

--- src/components/styledElements.js
+++ src/components/styledElements.js
@@ -1,10 +1,24 @@
+import React from 'react'
 import styled from 'styled-components'
+
+export const PaddedSection = ({bgColor="white", padding="3rem", children}) => (
+    <Section bgColor={bgColor} padding={padding}>
+        <Wrapper>
+            {children}
+        </Wrapper>
+    </Section>
+)
 
 export const Wrapper = styled.div`
     width: 90%;
     max-width: 1200px;
     height: 100%;
     margin: 0 auto;
+`
+
+export const Section = styled.section`
+    background-color: ${props => props.bgColor};
+    padding: ${props => props.padding} 0;
 `
 
 export const Flex = styled.div`
