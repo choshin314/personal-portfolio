ce2a69e99da4d8d827bac1db5d1fea6947de6b53
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-11-23 21:45:39 -0600

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shin Cho Portfolio`,
+    description: `Portfolio site for web developer Shin Cho.`,
+    author: `Shin Cho`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,20 +19,13 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-source-contentful`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        spaceId: `1pdyfvosxic1`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
---
+++ src/components/styledElements.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    width: 90%;
+    max-width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+`
+
+export const Flex = styled.div`
+    display: flex;
+`
