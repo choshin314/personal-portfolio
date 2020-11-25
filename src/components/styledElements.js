0d8351052bb735b56549aa1d13aa666316c6c07f
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-11-24 19:42:49 -0600

    rm media query css vars and move to object in styledEls (vars dont work for media queries)

--- src/components/styledElements.js
+++ src/components/styledElements.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+export const media = {
+    lgPhone: '600px',
+    tablet: '768px',
+    laptop: '1400px',
+    wide: '1800px'
+}
+
 export const PaddedSection = ({bgColor="white", padding="3rem", children}) => (
     <Section bgColor={bgColor} padding={padding}>
         <Wrapper>
@@ -23,4 +30,23 @@ export const Section = styled.section`
 
 export const Flex = styled.div`
     display: flex;
+`
+
+export const SectionTitle = styled.h2`
+    text-align: center;
+    font-weight: 700;
+    position: relative;
+    margin-bottom: 3rem;
+
+    &::after {
+        content: '';
+        width: 5rem;
+        position: absolute;
+        left: 50%;
+        bottom: -10px;
+        height: 4px;
+        border-radius: 2px;
+        background-color: var(--darkGrey);
+        transform: translateX(-50%);
+    }
 `
