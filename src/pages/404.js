e50f1664c39ea462290a160bec75ec1793d4b044
choshin314 <62952497+choshin314@users.noreply.github.com>
2020-12-14 20:44:57 -0600

    add helmet stuff

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
 import React from "react"
-
+import {Helmet} from 'react-helmet'
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({data}) => (
   <Layout>
-    <SEO title="404: Not found" />
+    <Helmet title={`404 Not Found | ${data.site.siteMetadata.title}`} >
+      <meta name="description" content="404 page for Shin Cho's portfolio" />
+    </Helmet>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
 export default NotFoundPage
+
+export const query = graphql`
+  query NotFoundPage {
+    site {
+      siteMetadata {
+        title 
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,37 @@
 import React from "react"
+import {graphql} from 'gatsby'
+import {Helmet} from 'react-helmet'
 
 import Layout from '../components/layout'
 import AboutSection from '../components/about/aboutSection.js'
 import ProjectsSection from '../components/projects/projectsSection.js'
 import ContactSection from '../components/contact/contactSection.js'
 
-const IndexPage = () => (
-  <Layout>
-    <AboutSection />
-    <ProjectsSection />
-    <ContactSection />
-  </Layout>
-)
+const IndexPage = ({data}) => {
+  const { site: { siteMetadata } } = data;
+  return (
+    <Layout>
+      <Helmet title={siteMetadata.title}>
+        <meta name="description" content={siteMetadata.description} />
+      </Helmet>
+      <AboutSection />
+      <ProjectsSection />
+      <ContactSection />
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query helmetData {
+    site {
+      siteMetadata {
+        title 
+        description
+      }
+    }
+  }
+`
+
+
